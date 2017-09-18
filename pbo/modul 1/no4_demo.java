8819a3ad5062907461daa3a1718deeb40f4ec3c8
ilham-hax0r <l200160073@student.ums.ac.id>
2017-09-18 21:47:22 +0700

    Praktikum PBO - Modul 1 - Nomor 4 (DEMO)

---
+++ pbo/modul 1/no4_demo.java
@@ -0,0 +1,13 @@
+/*
+ * @author Ilham Wicaksono
+ * @app : netbeans IDE 8.2
+ */
+ public class KucingkuDemo {
+    public static void main(String[] args){
+        Kucingku Kucing = new Kucingku();
+        
+        Kucing.meong("White");
+        Kucing.umur(6);
+        Kucing.infoKucing();
+    }
+}
