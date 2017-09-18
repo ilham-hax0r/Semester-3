7aa1f837b7cf2afdcf5dbb8d9a567e447b90cbc9
ilham-hax0r <l200160073@student.ums.ac.id>
2017-09-19 00:13:25 +0700

    Praktikum PBO - Modul 1 - Nomor 3 PR (Karyawan)

---
+++ pbo/modul 1/no3(pr)_Karyawan.java
@@ -0,0 +1,23 @@
+/*
+ * @author Ilham Wicaksono
+ * @app : netbeans IDE 8.2
+ */
+public class Karyawan {
+    String nama;
+    String alamat;
+    String jabatan;
+    double gaji;
+    
+    String tampilkanNama(){
+        return nama;
+    }
+    String tampilkanJabatan(){
+        return jabatan;
+    }
+    String tampilkanAlamat(){
+        return alamat;
+    }
+    double tampilkanGaji(){
+        return gaji;
+    }
+}
