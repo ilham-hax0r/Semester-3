06099172b7add789b303d97e54a20568730b8e49
ilham-hax0r <l200160073@student.ums.ac.id>
2017-09-19 00:15:06 +0700

    Praktikum PBO - Modul 1 - Nomor 3 PR (Mahasiswa)

---
+++ pbo/modul 1/no3(pr)_Mahasiswa.java
@@ -0,0 +1,23 @@
+/*
+ * @author Ilham Wicaksono
+ * @app : netbeans IDE 8.2
+ */
+ public class Mahasiswa {
+    String nama;
+    String nim;
+    String alamat;
+    int semester;
+    
+    String tampilkanNama(){
+        return nama;
+    }
+    String tampilkanNim(){
+        return nim;
+    }
+    String tampilkanAlamat(){
+        return alamat;
+    }
+    int tampilkanSemester(){
+        return semester;
+    }
+}
