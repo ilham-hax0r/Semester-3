b26d22aa2d50358d24772bb5e96f64acda50efdf
Ilham Wicaksono <l200160073@student.ums.ac.id>
2017-09-26 14:29:08 +0700

    Praktikum PBO - Modul 3 - Default Demo

---
+++ pbo/modul 3/DefaultDemo.java
@@ -0,0 +1,15 @@
+/*
+ * @author Ilham Wicaksono
+ * @app : netbeans IDE 8.2
+ */
+ /*
+ * Sama package dan bisa diakses karena terdapat dalam package yang sama
+ */
+package newpackage;
+class DefaultDemo {
+    public static void main(String[] args){
+        DefaultModifier default1 = new DefaultModifier();
+        
+        default1.jumlah();
+    }
+}
