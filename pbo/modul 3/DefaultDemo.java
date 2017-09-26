075b0f943b34d2d2d390d9f41100b4161209cbbb
Ilham Wicaksono <l200160073@student.ums.ac.id>
2017-09-26 14:19:14 +0700

    Praktikum PBO - Modul 3 - Default Demo (Beda pkg)

---
+++ pbo/modul 3/DefaultDemo.java
@@ -0,0 +1,16 @@
+/*
+ * @author Ilham Wicaksono
+ * @app : netbeans IDE 8.2
+ */
+ /*
+ * Beda package, package dalam soal praktikum.pbo.package1
+ * Tidak bisa di akses beda package dan harus dalam package yang sama
+ */
+package praktikum.pbo.package1;
+class DefaultDemo {
+    public static void main(String[] args){
+        DefaultModifier default1 = new DefaultModifier();
+        
+        default1.jumlah();
+    }
+}
