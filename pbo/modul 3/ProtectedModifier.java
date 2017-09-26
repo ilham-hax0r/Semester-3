0fbe642ba2ac871caf891274f561681aeaed7d3e
Ilham Wicaksono <l200160073@student.ums.ac.id>
2017-09-26 14:54:51 +0700

    Praktikum PBO - Modul 3 - Protected Modifier

---
+++ pbo/modul 3/ProtectedModifier.java
@@ -0,0 +1,13 @@
+/*
+ * @author Ilham Wicaksono
+ * @app : netbeans IDE 8.2
+ */
+package newpackage;
+class ProtectedModifier {
+    protected void printInfo(){
+        System.out.println("Protected Modifier");
+    }
+    protected void sendMessage(){
+        System.out.println("this is a message");
+    }
+}
