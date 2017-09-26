c42f464dd5211a788aba73fe25370de963356d91
Ilham Wicaksono <l200160073@student.ums.ac.id>
2017-09-26 15:40:00 +0700

    Praktikum Web Dinamis - Modul 3 - Halo

---
+++ web_dinamis/modul 3/halo.php
@@ -0,0 +1,15 @@
+/*
+ * @author Ilham Wicaksono
+ * @app : sublime text 3
+*/
+<html>
+	<head>
+		<title><?php echo 'Belajar PHP';?></title>
+	</head>
+	<body>
+		<?php
+		echo 'Halo, Dunia! </br>';
+		echo 'Ini <i>script</i> php pertamaku';
+		?>
+	</body>
+</html>
